eae6028f17d786665801e525b9556aaf851f5815
enikili <enikili69@gmail.com>
2025-04-23 03:28:54 +0300

    Delete ToDoList directory

--- ToDoList/ToDoList/migrations/20250221180448_messages.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tasks (
-    id SERIAL PRIMARY KEY,
-    task VARCHAR(511) NOT NULL,
-    is_done BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    deleted_at TIMESTAMP DEFAULT NULL
-);
-
